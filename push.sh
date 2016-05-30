4baa619ba99f4e534759e86496517e1b610edcfb
Damien Cassou <damien@cassou.me>
2016-05-30 10:42:18 +0200

    Use rsync to push

--- push.sh
+++ push.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
-SERVER=files.pharo.org
-BASEDIR=/appli/files.pharo.org/books/
-SERVER_USER=filepharosync
-TMP_DIR='~cassou/books-website'
-
-ssh $SERVER sh <<EOF
-sudo su --login --command "mkdir -p \"$BASEDIR\"" "$SERVER_USER"
-rm -rf $TMP_DIR
-mkdir $TMP_DIR
-EOF
-
-echo Pushing all files to $SERVER
-scp -r * $SERVER:$TMP_DIR
-
-ssh $SERVER sh <<EOF
-sudo su --login --command "cp -R $TMP_DIR/* $BASEDIR" $SERVER_USER
-EOF
+echo Uploading files to files.pharo.org...
+rsync --progress --cvs-exclude --recursive --links --times --delete . filepharosync@files.pharo.org:/appli/files.pharo.org/books
+echo done
