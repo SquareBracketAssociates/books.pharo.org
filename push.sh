a9a4da3a349d65d7e6a2a3b3ea8352fc79a1b0c6
StephaneDucasse <stephane.ducasse@inria.fr>
2018-01-24 09:20:26 +0100

    adding call stack

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo Uploading files to files.pharo.org...
-rsync --progress --cvs-exclude --recursive --links --times --delete . filepharosync@files.pharo.org:/appli/files.pharo.org/books
+#rsync --progress --cvs-exclude --recursive --links --times --delete . #filepharosync@files.pharo.org:/appli/files.pharo.org/books
+rsync --progress --cvs-exclude --recursive --links --times --delete . pharoorgde@files.pharo:books
 echo done
