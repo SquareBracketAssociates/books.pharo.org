7900d7925f8a6a92d771f6b8eb81d2c4ad82220e
StéphaneDucasse <stephane.ducasse@inria.fr>
2018-08-27 10:33:44 +0200

    updating pheme

--- push.sh
+++ push.sh
@@ -4,3 +4,6 @@ echo Uploading files to files.pharo.org...
 #rsync --progress --cvs-exclude --recursive --links --times --delete . #filepharosync@files.pharo.org:/appli/files.pharo.org/books
 rsync --progress --cvs-exclude --recursive --links --times --delete . pharoorgde@files.pharo:books
 echo done
+
+
+echo The pdfs are hosted under /pharoorgde/files/book-pdfs
